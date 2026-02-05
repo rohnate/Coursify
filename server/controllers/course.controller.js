56a75c7a33156a663f785dfb9336b2686bc9ced6
itsRss <164704500+rohnate@users.noreply.github.com>
2026-02-05 10:15:13 +0000

    updated course controllers

--- server/controllers/course.controller.js
+++ server/controllers/course.controller.js
@@ -1,7 +1,43 @@
 const cModel = require("../models/course.model");
+const pModel = require("../models/purchase.model");
+const jwt = require("jsonwebtoken");
 
-function courses() {}
-function myCourses() {}
+async function courses(req, resp, next) {
+  try {
+    const allCourses = await cModel.find();
+    req.courses = allCourses;
+    next();
+  } catch (e) {
+    resp.status(500).json({
+      message: "Failed to fetch courses",
+      error: e.message,
+    });
+  }
+}
+
+async function myCourses(req, resp, next) {
+  const token = req.headers.authorization;
+
+  try {
+    const userDetail = jwt.verify(token, process.env.JWT_USER_SECRET);
+    const userId = userDetail.id;
+
+    const userPurchase = await pModel.find({ userId });
+    if (userPurchase.length === 0) {
+      return resp.status(404).json({
+        message: "User has not purchased any course.",
+      });
+    }
+    req.userPurchase = userPurchase;
+    next();
+  } catch (e) {
+    return resp.status(403).json({
+      message: "Error while fetching your courses. Please try again",
+      error: e.message,
+    });
+  }
+}
+
 function courseContent() {}
 
 module.exports = {
--- server/routes/course.route.js
+++ server/routes/course.route.js
@@ -1,16 +1,35 @@
 const { Router } = require("express");
 const cRouter = Router();
+const {
+  courses,
+  myCourses,
+  courseContent,
+} = require("../controllers/course.controller");
+const { jwtVerifyUserMiddleware } = require("../middlewares/user.middleware");
 
-cRouter.get("/courses", function (req, resp) {
-  resp.send("courses page");
+cRouter.get("/", courses, function (req, resp) {
+  const allCourses = req.courses;
+  resp.json(allCourses);
 });
 
-cRouter.get("/mycourses", function (req, resp) {
-  resp.send("mycourses page");
-});
+cRouter.get(
+  "/mycourses",
+  jwtVerifyUserMiddleware,
+  myCourses,
+  function (req, resp) {
+    // for now, this route will only work for user not for admin.
+    const userCourses = req.userPurchase;
+    resp.json(userCourses);
+  },
+);
 
-cRouter.get("/course-content", function (req, resp) {
-  resp.send("course-content");
-});
+cRouter.get(
+  "/course-content",
+  jwtVerifyUserMiddleware,
+  courseContent,
+  function (req, resp) {
+    resp.send("course-content");
+  },
+);
 
 module.exports = cRouter;
